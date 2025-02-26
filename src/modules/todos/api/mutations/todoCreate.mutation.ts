14bfd6d6b44deab4276b4ea6f4b3b1f8b527d4aa
HarleenBhandalPXL <harleen.bhandal@student.pxl.be>
2025-02-26 11:56:00 +0100

    project: creating Todo's completed

--- src/modules/todos/api/mutations/todoCreate.mutation.ts
+++ src/modules/todos/api/mutations/todoCreate.mutation.ts
@@ -5,14 +5,13 @@ import type { TodoCreateForm } from '@/models/todo/create/todoCreateForm.model'
 
 import { TodoService } from '../services/todo.service'
 
-export function useSettingRoleCreateMutation(): UseMutationReturnType<TodoCreateForm, void> {
+export function useTodoCreateMutation(): UseMutationReturnType<TodoCreateForm, void> {
   return useMutation<TodoCreateForm, void>({
     queryFn: async ({ body }) => {
       await TodoService.create(body)
     },
     queryKeysToInvalidate: {
-      permissions: {},
-      roles: {},
+      todoIndex: {},
     },
   })
 }
--- src/modules/todos/features/overview/views/TodoOverviewView.vue
+++ src/modules/todos/features/overview/views/TodoOverviewView.vue
@@ -1,60 +1,76 @@
-<script lang="ts">
-import type { PaginationOptions } from '@wisemen/vue-core'
-import {
-  defineComponent,
-  onMounted,
-  ref,
-} from 'vue'
+<script setup lang="ts">
+import type {
+  PaginationOptions,
+  VcButtonProps,
+} from '@wisemen/vue-core'
+import { useDialog, VcButton } from '@wisemen/vue-core'
+import { onMounted, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+import AppPage from '@/components/layout/AppPage.vue'
 import type { TodoIndex } from '@/models/todo/index/todoIndex.model'
 import type { TodoIndexFilters } from '@/models/todo/index/todoIndexFilters.model'
 import { TodoService } from '@/modules/todos/api/services/todo.service'
 
-export default defineComponent({
-  name: 'TodoOverviewView',
-  setup() {
-    const todos = ref<TodoIndex[]>([])
+const variants: VcButtonProps['variant'][] = [
+  'default',
+]
+const todos = ref<TodoIndex[]>([])
 
-    async function fetchTodos(): Promise<void> {
-      const paginationOptions: PaginationOptions<TodoIndexFilters> = {
-        pagination: {
-          limit: 10,
-          offset: 0,
-        },
-      }
+const i18n = useI18n()
 
-      try {
-        const response = await TodoService.getAll(paginationOptions)
+async function fetchTodos(): Promise<void> {
+  const paginationOptions: PaginationOptions<TodoIndexFilters> = {
+    pagination: {
+      limit: 10,
+      offset: 0,
+    },
+  }
 
-        todos.value = response.data
-      }
-      catch (error) {
-        console.error('Error fetching todos:', error)
-      }
-    }
+  try {
+    const response = await TodoService.getAll(paginationOptions)
 
-    onMounted(fetchTodos)
+    todos.value = response.data
+  }
+  catch (error) {
+    console.error('Error fetching todos:', error)
+  }
+}
 
-    return {
-      todos,
-    }
-  },
+onMounted(fetchTodos)
+
+const addTodoCreateDialog = useDialog({
+  component: () => import('@/modules/todos/components/TodoCreateDialog.vue'),
 })
+
+function onAddTodoCreateDialog(): void {
+  addTodoCreateDialog.open({
+    id: 'addTodo',
+  })
+}
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li
-        v-for="todo in todos"
-        :key="todo.id"
-      >
-        {{ todo.title }}
-      </li>
-    </ul>
-  </div>
+  <AppPage :title="i18n.t('module.todo.title')">
+    <div>
+      <ul v-if="todos.length > 0">
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.title }}
+        </li>
+      </ul>
+      <p v-else>
+        {{ i18n.t('module.todo.no_todos') }}
+      </p>
+    </div>
+
+    <div class="grid grid-cols-3 gap-8">
+      <VcButton v-for="variant in variants" :key="variant" :variant="variant" @click="onAddTodoCreateDialog">
+        {{ i18n.t('module.todo.addbutton.text') }}
+      </VcButton>
+    </div>
+  </AppPage>
 </template>
 
-  <style scoped>
-  /* Add your styles here */
-  </style>
+<style scoped>
+/* Add your styles here */
+</style>
